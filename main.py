9d2a4794802014d1834d445f2904b460469a1584
Milen <feniks9511@gmail.com>
2022-06-03 17:07:56 +0300

    commiting change

--- main.py
+++ main.py
@@ -1,23 +1,6 @@
-# from js import document
 from datetime import datetime, timedelta
 
-#
-#
-# def run(*ags, **kws):
-#     tag_name = document.getElementById('tag_name').value;
-#     day = document.getElementById('day').value;
-#     month = document.getElementById('month').value;
-#     year = document.getElementById('year').value;
-#
-#     date = datetime.date(day=int(day), month=int(month), year=int(year))
-#
-#     new_date = date + datetime.timedelta(day + 45)
-#     due_date = f'{due_date.day}/{due_date.month}/{due_date.year} -/{tag_name}'
-#
-#     pyscript.write("due_date", due_date)
-
 tag_name = input("ENTER YOUR TAG NAME: ")
-
 
 # print("Insert date day/month/year")
 
@@ -30,14 +13,3 @@ date = datetime(day=day, month=month, year=year)
 due_date = date + timedelta(days=45)
 
 print(f"DUE DATE IS: {due_date.day}/{due_date.month}/{due_date.year} -/{tag_name}")
-
-# def date(day, month, year):
-#     date_day = datetime.date(day=int(day))
-#     month = datetime.date(month=int(month))
-#     year = datetime.datetime(year=int(year))
-#     return datetime.date
-
-
-# def count_date(value):
-#     value = datetime.timedelta(value)
-#     return f'{value}'
